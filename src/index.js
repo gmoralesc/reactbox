b6e948ec0da992dd9fb98de7f960e61525793a4d
Gustavo Morales <gustavo.morales@treasure-data.com>
2021-05-27 18:53:53 -0700

    Add Item component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 
 const root = document.getElementById("app");
 
+const Item = ({ title, subtitle }) => {
+  return (
+    <span>
+      {title} - {subtitle}
+    </span>
+  );
+};
+
 const element = (
-  <div className="container">
-    <p>Hello world</p>
-  </div>
+  <>
+    <Item title="Smells Like Teen Spirit" subtitle={<strong>Nirvana</strong>} />
+    <br />
+    <Item title="Nookie" subtitle="Limp Bizkit" />
+  </>
 );
 
 ReactDOM.render(element, root);
